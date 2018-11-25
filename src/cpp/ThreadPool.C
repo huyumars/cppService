1e1bd248e7662da0e13df3718887668a301c2c77
huyumars <huyumars@gmail.com>
2018-11-25 18:46:36 +0800

    safe exit

--- src/cpp/ThreadPool.C
+++ src/cpp/ThreadPool.C
@@ -1,13 +1,20 @@
 #include "ThreadPool.H"
+#include <chrono>
 
 void Executor::join(){
   _thread.join();
 }
 
 void Executor::run(){
-  while(true){
+  while(running){
     std::unique_lock<std::mutex> lock(cv_m);
     cv.wait(lock,[this]{return !_task_list.empty();});
     _task_list.apply();
   }
+}
+
+
+Executor::~Executor(){
+  running = false;
+  join();
 }
--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,16 +1,29 @@
 #include<iostream>
 #include "ThreadPool.H"
 
+class A {
+  public:
+    int i;
+    A(int _i):i(_i){};
+    A(const A& a):i(a.i){
+      std::cout<<"copied"<<std::endl;
+    }
+    A( A&& a):i(std::move(a.i)){
+      std::cout<<"moved"<<std::endl;
+    }
 
-void f(int i ){
-  std::cout<<i<<std::endl;
+};
+
+void f(A a ){
+  std::cout<<a.i<<std::endl;
 }
 
 int main(){
   Executor e;
-  for(int i=0; i< 100; ++i){
-    e.task([]{std::cout<<"jjj"<<std::endl;});
+  std::future<void> res;
+  for(int i=9; i< 10; ++i){
+    res = e.task(f,A(i));
   }
-  e.join();
+  res.wait();
   return 0;
 }
