9245e5e49557e4aea8ef7f589f9cd1920590a777
mars <huyumars@gmail.com>
2018-11-27 18:45:14 +0800

    log to cout

--- src/cpp/Logger.C
+++ src/cpp/Logger.C
@@ -8,11 +8,62 @@ void LogMsg::Log(const std::string & msg_,
                     const std::string & file_,
                     LogType level_){
   Ptr msgPtr = std::make_shared<LogMsg>(msg_,line_,file_,level_);
-  Singleton<Logger>::instance().send(msgPtr);
+  Logger::send(msgPtr);
+}
+
+Logger::Logger():_running(true), _thread([this]{ run(); }){ }
+
+void Logger::addDest(const LogDest & d){
+  _dests.push_back(d);
+}
+
+void Logger::run(){
+  do{
+    Logger::MsgQueue::QueuePtr queuePtr; 
+    _queue.getQueue(queuePtr);
+    for(LogDest & dest : _dests){
+      if(queuePtr){
+        while(queuePtr->size()){
+          dest.dispatch(queuePtr->front());
+          queuePtr->pop();
+        }
+      }
+    }
+  }while(_running);
+}
+
+Logger::~Logger(){
+  _running = false;
+  join();
 }
 
 void Logger::send(LogMsg::Ptr msgPtr){
-  std::cout<<(*msgPtr);
+  Singleton<Logger>::instance()._sendAync(msgPtr);
+}
+
+
+void Logger::_sendAync(LogMsg::Ptr msgPtr){
+  _queue.put(msgPtr);
+}
+
+void console(std::ostream& os, LogMsg::Ptr msgPtr){
+  if(msgPtr) os<<(*msgPtr);
+}
+
+LogDest::LogDest(Type type){
+  switch(type){
+    case Type::stderr:
+      _dispatchFunctor = [](LogMsg::Ptr ptr){
+        console(std::cerr, ptr);
+      };
+      break;
+    case Type::stdout:
+    default:
+      _dispatchFunctor = [](LogMsg::Ptr ptr){
+        console(std::cout, ptr);
+      };
+      break;
+  }
 }
 
 std::ostream & operator<<(std::ostream &os, const LogType& level){
